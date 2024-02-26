c6663a8099967c631e9ca69b114237f28909a7d7
Isaias Tavares <isaiasengsoft@gmail.com>
2024-02-25 23:22:15 -0300

    Create ssr-form-script-2.js

---
+++ ssr-form-script-2.js
@@ -0,0 +1,88 @@
+const formToStepMapping = {
+    "1aa90452-a352-4bce-848b-645ce088b9f7": 1, // 1 Requirements
+    "1847fccb-4e07-46fd-b987-5abd688303d2": 1, // 1.1 Requirements
+    "b1ded006-4c7d-4192-ba18-a08eeda6081c": 2, // 2 Your Info
+    "d6ed34de-f6d8-4408-8f8f-0472d4ceee59": 3  // 3 Get advice
+};
+
+const portalId = '45145570';
+const target = '#multistep-form';
+const solutionField = 'software_type_requested';
+const employeeField = "0-2/employee_number"
+
+const data = [];
+const options = [];
+const solutionValues = [];
+const formKeys = Object.keys(formToStepMapping);
+
+const updateStepBar = (currentStep) => {
+    const stepElements = document.querySelectorAll('.step');
+    stepElements.forEach((stepElement, index) => {
+        if (index + 1 === currentStep) {
+            stepElement.classList.add('active');
+        }
+    });
+};
+
+const addCompletedClass = (step) => {
+    const stepElements = document.querySelectorAll('.step');
+    stepElements.forEach((stepElement, index) => {
+        if (index + 1 === step) {
+            stepElement.classList.add('completed');
+        }
+    });
+};
+
+const generateFormOptions = (form, index) => {
+    return {
+        portalId,
+        formId: form,
+        target,
+        onFormReady: function(form) {
+            if (index === 2) {
+                form.find('.hs_' + solutionField).hide();
+                form.find('input[name="' + employeeField + '"]').val(data[0].value).change();
+
+                solutionValues.forEach(value => {
+                    form.find('input[name="' + solutionField + '"][value="' + value + '"]').prop('checked', true);
+                });
+            }
+        },
+        onFormSubmit: function(form) {
+            if (index === 0) {
+                const incoming = $(form).serializeArray();
+                data.push(incoming[0]);
+            }
+            if (index === 1) {
+                const incoming = $(form).serializeArray();
+                solutionValues.push(...incoming
+                    .filter(item => item.name === solutionField)
+                    .map(item => item.value));
+            }
+        },
+        onFormSubmitted: function() {
+            if (index < formKeys.length - 1) {
+                hbspt.forms.create(options[index + 1]);
+                
+                const nextForm = formKeys[index + 1];
+                const nextStep = formToStepMapping[nextForm];
+                if (nextStep) {
+                    updateStepBar(nextStep);
+                }
+            }
+            addCompletedClass(index);
+        }
+    };
+};
+
+const multiStepForm = () => {
+    formKeys.forEach((form, index) => {
+        options.push(generateFormOptions(form, index));
+    });
+
+    updateStepBar(1);
+
+    hbspt.forms.create(options[0]);
+};
+
+multiStepForm();
