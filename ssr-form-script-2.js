8c13a78cc835d480fe72db4878ac9164929d75fd
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-18 00:19:13 -0300

    adjust timer text remaining

--- ssr-form-script-2.js
+++ ssr-form-script-2.js
@@ -2,22 +2,22 @@ const formInformation = {
     "1aa90452-a352-4bce-848b-645ce088b9f7": {
         step: 1,  // 1 Requirements
         progressBarPercentage: 0,
-        timeRemaining: "60 seconds left..."
+        timeRemaining: "60"
     },
     "1847fccb-4e07-46fd-b987-5abd688303d2": {
         step: 1,  // 1.1 Requirements
         progressBarPercentage: 25,
-        timeRemaining: "45 seconds left..."
+        timeRemaining: "45"
     },
     "b1ded006-4c7d-4192-ba18-a08eeda6081c": {
         step: 2, // 2 Your Info
         progressBarPercentage: 50,
-        timeRemaining: "30 seconds left..."
+        timeRemaining: "30"
     },
     "d6ed34de-f6d8-4408-8f8f-0472d4ceee59": {
         step: 3, // 3 Get advice
         progressBarPercentage: 75,
-        timeRemaining: "15 seconds left..."
+        timeRemaining: "15"
     }
 };
 
@@ -42,7 +42,7 @@ const updateStepBar = (currentStep, nextForm) => {
     const progressBar = document.getElementById('progress-bar');
     const progressBarFilled = document.getElementById('progress-bar-filled');
     const progressText = document.getElementById('progress-text');
-    const timerText = document.getElementById('timer-text');
+    const timerText = document.getElementById('timer-text-replaced');
 
     if (nextForm) {
         const percentage = formInformation[nextForm].progressBarPercentage;
