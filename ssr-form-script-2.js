528013e568bf25ddb3f8805c6132e1dd322dd179
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-19 00:10:36 -0300

    data push

--- ssr-form-script-2.js
+++ ssr-form-script-2.js
@@ -118,6 +118,7 @@ const generateFormOptions = (form, index) => {
             if (index === 2) {
                 const form3 = $(form).serializeArray();
                 console.log(form3);
+                data.push(form3)
             }
         },
         onFormSubmitted: function() {
