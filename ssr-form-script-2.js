cf899265bf0574148153c0b509fe59cda3c93550
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-25 00:49:38 -0300

    test 13

--- ssr-form-script-2.js
+++ ssr-form-script-2.js
@@ -123,8 +123,8 @@ const generateFormOptions = (form, index) => {
                     createFormAndUpdateProgressBar(nextForm, index + 1);
                 }
             } else {
-                const meetingsDivElement = createDivElement('meetings-iframe-container', 'https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true');
-                const meetingsScriptElement = createScriptElement('https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js');
+                //const meetingsDivElement = createDivElement('meetings-iframe-container', 'https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true');
+                //const meetingsScriptElement = createScriptElement('https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js');
 
                 console.log($(form));
                 //console.log($(form).querySelector('.submitted-message'));
@@ -139,8 +139,8 @@ const generateFormOptions = (form, index) => {
                 //console.log(submittedMessageDiv.length)
                 //console.log(submittedMessageDiv2.length)
 
-                //const meetingsDivElement = $('<div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true"></div>');
-                //const meetingsScriptElement = $('<script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>');
+                const meetingsDivElement = $('<div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true"></div>');
+                const meetingsScriptElement = $('<script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>');
 
                 $(form).append(meetingsDivElement).append(meetingsScriptElement);
                 //submittedMessageDiv.append(meetingsDivElement).append(meetingsScriptElement);
