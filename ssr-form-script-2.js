4ebad62c911303765e92c8e523de3bd825d13611
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-25 00:39:23 -0300

    test 10

--- ssr-form-script-2.js
+++ ssr-form-script-2.js
@@ -130,21 +130,24 @@ const generateFormOptions = (form, index) => {
                 //console.log($(form).querySelector('.submitted-message'));
                 //console.log($(form).find('.submitted-message'));
 
-                const submittedMessageDiv = form.find('div[class="submitted-message"]')
+                const submittedMessageDiv = $(form).find('div.submitted-message.hs-main-font-element.hs-form-d6ed34de-f6d8-4408-8f8f-0472d4ceee59.hs-form-d6ed34de-f6d8-4408-8f8f-0472d4ceee59_18a6b80c-63cd-42e2-aa40-ba24a0136760')
                 const submittedMessageDiv2 = $(form).find('.submitted-message')
 
                 console.log('Submitted Message Div 1: ', submittedMessageDiv);
                 console.log('Submitted Message Div 2: ', submittedMessageDiv2);
 
+                console.log(submittedMessageDiv.length)
                 console.log(submittedMessageDiv2.length)
 
                 const meetingsDivElement = $('<div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true"></div>');
                 const meetingsScriptElement = $('<script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>');
 
+                submittedMessageDiv.append(meetingsDivElement).append(meetingsScriptElement);
                 submittedMessageDiv2.append(meetingsDivElement).append(meetingsScriptElement);
                 
                 $(document).ready(function() {
                     console.log('código executado');
+                    submittedMessageDiv.append(meetingsDivElement).append(meetingsScriptElement);
                     submittedMessageDiv2.append(meetingsDivElement).append(meetingsScriptElement);          
                 });
 
