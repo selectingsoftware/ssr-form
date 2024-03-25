299fb322fc686221bc8162f438bfd966ec14c056
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-25 00:40:45 -0300

    test 11

--- ssr-form-script-2.js
+++ ssr-form-script-2.js
@@ -142,11 +142,13 @@ const generateFormOptions = (form, index) => {
                 const meetingsDivElement = $('<div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/zach-mason/zach-advisor-calls?embed=true"></div>');
                 const meetingsScriptElement = $('<script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>');
 
+                $(form).append(meetingsDivElement).append(meetingsScriptElement);
                 submittedMessageDiv.append(meetingsDivElement).append(meetingsScriptElement);
                 submittedMessageDiv2.append(meetingsDivElement).append(meetingsScriptElement);
                 
                 $(document).ready(function() {
                     console.log('código executado');
+                    $(form).append(meetingsDivElement).append(meetingsScriptElement);
                     submittedMessageDiv.append(meetingsDivElement).append(meetingsScriptElement);
                     submittedMessageDiv2.append(meetingsDivElement).append(meetingsScriptElement);          
                 });
